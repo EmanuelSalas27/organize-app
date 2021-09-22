5370eb551bbc91494181a3b3e67dc8c0d000a67c
Emanuel <emanuelsalasdeleon@hotmail.com>
2021-09-22 00:10:46 -0500

    clean code

--- organize.js
+++ organize.js
@@ -20,7 +20,7 @@ function organize(finalPath, extensions){
        }
 
     })
-console.log(filterFiles , extensions)
+
     //Crear carpetas para las extensiones
     extensions.forEach((ext)=>{
         if(!fs.existsSync( path.join(organizeDir,`${ext}`))){
@@ -48,73 +48,5 @@ module.exports = {
 
 
 
-
-
-
-//Leer archivos de organize
-// const files = fs.readdirSync(path.join(__dirname, 'organize'), 'utf-8')
-
-// //console.log(files)
-// console.log( files)
-
-// getFiles(files)
-
-// function getFiles(files){
-//     const txt = files.filter((e)=>e.includes('.txt'))
-//     const jpg = files.filter((e)=>e.includes('.jpg'))
-//     const mp4 = files.filter((e)=>e.includes('.mp4'))
-//     const pdf = files.filter((e)=>e.includes('.pdf'))
-//     const doc = files.filter((e)=>e.includes('.doc'))
-
-//     //Checar si hay archivos y crear carpetas
-//     txt.length >=1 &&(
-//         !fs.existsSync('./organize/txt')&&
-//         fs.mkdirSync('./organize/txt')
-//     )
-
-//     jpg.length >=1 &&(
-//         !fs.existsSync('./organize/jpg')&&
-//         fs.mkdirSync('./organize/jpg')
-//     )
-
-//     mp4.length >=1 &&(
-//         !fs.existsSync('./organize/mp4')&&
-//         fs.mkdirSync('./organize/mp4')
-//     )
-
-//     pdf.length >=1 &&(
-//         !fs.existsSync('./organize/pdf')&&
-//         fs.mkdirSync('./organize/pdf')
-//     )
-
-//     doc.length >=1 &&(
-//         !fs.existsSync('./organize/doc')&&
-//         fs.mkdirSync('./organize/doc')
-//     )
-
-
-
-
-//     //Mover objetos a su carpeta
-//         txt.forEach(e => {
-//             fs.renameSync(`./organize/${e}`,`./organize/txt/${e}`)
-//         });
-
-//         jpg.forEach(e => {
-//             fs.renameSync(`./organize/${e}`,`./organize/jpg/${e}`)
-//         });
-
-//         mp4.forEach(e => {
-//             fs.renameSync(`./organize/${e}`,`./organize/mp4/${e}`)
-//         });
-
-//         pdf.forEach(e => {
-//             fs.renameSync(`./organize/${e}`,`./organize/pdf/${e}`)
-//         });
-
-//         doc.forEach(e => {
-//             fs.renameSync(`./organize/${e}`,`./organize/doc/${e}`)
-//         });
-//     }
 
 
